e94bc75615d7e8950952187aa6255b9bb1539e12
Matthias Kannwischer <m.j.kannwischer@surrey.ac.uk>
2017-11-15 15:13:50 +0000

    experiment

--- experiment_new_3.sh
+++ experiment_new_3.sh
@@ -5,9 +5,9 @@ for (( i=0; i<=10000; i++))
 do
   
 
-  for k in 20 30 40 50;
+  for k in 5 10 15 20 30 40 50;
   do
-    for p in 2 4 8 16 32 64;
+    for p in 1 2 4 8 16 32 64;
     do
       file="data_new/P_N1/N_32/k_"$k"_p_"$p".log"
       ./attack2 32 10 5 $p $k --silent >> $file
--- plots/FA_new.py
+++ plots/FA_new.py
@@ -33,8 +33,9 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 import matplotlib.pyplot as plt
+from itertools import product
 
-def parse_csv(i):
+def parse_csv_exp1(i):
     ifile = open("../data_new/P_1/N_32/%d.log"%i, "rb")
     reader = csv.reader(ifile, delimiter=';')
     values = []
@@ -42,6 +43,16 @@ def parse_csv(i):
         values.append(int(row[10]))
     ifile.close()
     return values
+
+def parse_csv_exp2(k, p):
+    ifile = open("../data_new/P_N1/N_32/k_%d_p_%d.log"%(k,p), "rb")
+    reader = csv.reader(ifile, delimiter=';')
+    values = []
+    for row in reader:
+        values.append(int(row[10]))
+    ifile.close()
+    return values
+
 
 def plot_lines(x, exp, theo, fname):
     f = plt.figure(figsize=(11,5))
@@ -73,9 +84,36 @@ def prob(W, ell,q):
 
 
 # 1 means failure; 0 is success
-data = map(lambda(l): (sum(l), len(l)), map(lambda (i): parse_csv(i), xrange(1,111)))
+data = map(lambda(l): (sum(l), len(l)), map(lambda (i): parse_csv_exp1(i), xrange(1,111)))
 exp_results = map(lambda (fail, all): (float(all)-fail)/all, data)
 theoretical_results = map(lambda(q): prob(16, 67, q), xrange(1,111))
 
 plot_lines(range(1,111), exp_results, theoretical_results, "FA_new_N32.pdf")
 
+
+ks = [5, 10, 15, 20, 30, 40, 50]
+ps = [1, 2, 4, 8, 16, 32, 64]
+tuples = map(lambda(k, p): (k,p),  product(ks,ps))
+data   = map(lambda(k,p): (k, p, parse_csv_exp2(k,p)), tuples)
+exp_results = map(lambda(k,p, fail, all): (k,p, (float(all)-fail)/all), map(lambda (k,p,l): (k,p,sum(l), len(l)), data))
+
+lines = map(lambda(k): filter(lambda(ki, p, prob): ki == k , exp_results), ks)
+
+def plot_lines_2(lines, fname):
+    f = plt.figure(figsize=(11,5))
+    plt.ylim(ymin=-0.1)
+    plt.ylim(ymax=1.1)
+    for line in lines:
+        x = map(lambda(k,p,prob): p, line)
+        y = map(lambda(k,p,prob): prob, line)
+        plt.plot(x,y, ".", ms=9.0,color="grey")
+        plt.plot(x,y,label="k=%d"%line[0][0])
+        
+        
+    legend = plt.legend(loc='lower right', shadow=True, fontsize='x-large')
+    plt.ylabel("success probability")
+    plt.xlabel("number of forgery attempts p")
+    plt.tight_layout()
+    f.savefig(fname)
+
+plot_lines_2(lines, "FA_new_PN1.pdf")
